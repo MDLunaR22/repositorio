7b1aaf21eb5478b2c3f2e4d6fb8a1ada1c880a72
mluna-2021312 <mluna-2021312@kinal.edu.gt>
2023-01-18 14:52:36 -0600

    Primer commit v1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mi primer respositorio en internet
+
+Saludos a todos gente :D
---
+++ index.js
@@ -0,0 +1,6 @@
+function getClick() {
+    const a = document.getElementById("valor1").value;
+    const b = document.getElementById("valor2").value;
+    const resultado = parseInt(a) + parseInt(b);
+    console.log(resultado);
+}
